41a11d95ddd3e4b0e5de36c58ba44039cd1c317e
Tanuj <rockstart22005@gmail.com>
2026-08-02 10:49:14 +0530

    Time: 3 ms (17.92%), Space: 8.6 MB (15.08%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        if(x == 0 || x == 1) return x;
-        int ans = 0;
-        for(int i=0;i<=x/2;i++){
-            if( 1LL*i*i <= x){
-                ans = i;
-            }else{
-                break;
-            }
+        if (x < 2) return x;
+
+        int left = 1, right = x / 2;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            long long sq = 1LL * mid * mid;
+
+            if (sq == x)
+                return mid;
+            else if (sq < x)
+                left = mid + 1;
+            else
+                right = mid - 1;
         }
-        return ans;
+        return right;
     }
 };
