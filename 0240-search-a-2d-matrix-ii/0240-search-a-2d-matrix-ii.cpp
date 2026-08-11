42b0344b37780099de4de3ec790918a31d04a075
Tanuj <rockstart22005@gmail.com>
2026-08-11 22:46:52 +0530

    Time: 47 ms (82.17%), Space: 18.8 MB (39.93%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -4,11 +4,15 @@ public:
         int n = matrix.size();
         int m = matrix[0].size();
 
-        for(int i = 0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(matrix[i][j] == target){
-                    return true;
-                }
+        int row = 0;
+        int colmn = m-1;
+        while(row<n && colmn >=0){
+            if(matrix[row][colmn] == target){
+                return true;
+            }else if(matrix[row][colmn] < target){
+                row++;
+            }else{
+                colmn--;
             }
         }
         return false;
