4bf7f4a45feecf10168bb714741542621913cd8b
Tanuj <rockstart22005@gmail.com>
2026-08-18 11:02:56 +0530

    Time: 52 ms (80.77%), Space: 23.9 MB (56.38%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -8,46 +8,30 @@
  */
 class Solution {
 public:
-    int getLength(ListNode* node) {
-        int length = 0;
-
-        while (node != NULL) {
-            length++;
-            node = node->next;
-        }
-
-        return length;
-    }
-
     ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
-        int len1 = getLength(headA);
-        int len2 = getLength(headB);
+        if (headA == NULL || headB == NULL) {
+            return NULL;
+        }
 
         ListNode* t1 = headA;
         ListNode* t2 = headB;
 
-        // Move the longer list ahead
-        if (len1 > len2) {
-            for (int i = 0; i < len1 - len2; i++) {
-                t1 = t1->next;
-            }
-        }
-        else {
-            for (int i = 0; i < len2 - len1; i++) {
-                t2 = t2->next;
-            }
-        }
+        while (t1 != t2) {
 
-        // Move both pointers together
-        while (t1 != NULL && t2 != NULL) {
-            if (t1 == t2) {
-                return t1;
-            }
+    if (t1 == NULL) {
+        t1 = headB;
+    }
+    else {
+        t1 = t1->next;
+    }
 
-            t1 = t1->next;
-            t2 = t2->next;
-        }
-
-        return NULL;
+    if (t2 == NULL) {
+        t2 = headA;
+    }
+    else {
+        t2 = t2->next;
+    }
+}
+        return t1;
     }
 };
